8a8de8bc66d37169f04ada0cebcea69d48070594
Zelipha <zeliphawambui756@gmail.com>
2022-07-22 03:53:14 +0300

    N queens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -3,7 +3,7 @@
 Determines all possible solutions to placing N
 N non-attacking queens on an NxN chessboard.
 Example:
-    $ ./101-nqueens.py N
+    $ ./0-nqueens.py N
 N must be an integer greater than or equal to 4.
 Attributes:
     board (list): A list of lists representing the chessboard.
