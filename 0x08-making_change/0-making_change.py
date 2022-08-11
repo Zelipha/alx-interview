a4017afefa3752fc487f006bcd7037bbe8bf64ed
Zelipha <zeliphawambui756@gmail.com>
2022-08-12 02:29:12 +0300

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A function to determine the fewest number of coins needed to meet a given amount total"""
+"""A function to determine the fewest number of coins needed
+   to meet a given amount total"""
 
 
 def makeChange(coins, total):
